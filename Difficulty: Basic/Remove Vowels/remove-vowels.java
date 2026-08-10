8928e4cf93276fe8c582b1895c1de9ffd0ff4426
Arun <kumararungobi@gmail.com>
2026-08-11 04:29:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Vowels/remove-vowels.java
+++ Difficulty: Basic/Remove Vowels/remove-vowels.java
@@ -1,16 +1,7 @@
 class Solution {
     String removeVowels(String s) {
         // code here
-        // int len = s.length();
-        // char[] a = s.toCharArray();
-        // String[] ans = {};
-        
-        // char[] ch = {'a','e','i','o','u','A','E','I','O','U'};
-        // for(int i=0 ; i<len ; i++){
-        //     if(ch[i] == a[i]){
-        //         ans = add + (a[i]);
-        //     }
-        // }return ans;
+       
         return s.replaceAll("[aeiouAeiou]","");
     }
 }
