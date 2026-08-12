125d20ce5af6df272a603085a7d22d5cf256a714
Arun <kumararungobi@gmail.com>
2026-08-12 12:10:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Cube Root/cube-root.java
+++ Difficulty: Basic/Cube Root/cube-root.java
@@ -1,9 +1,9 @@
 class Solution {
     static int cubeRoot(int n) {
         // code here
-        float r =(float) 1/3;
-        
-        int s =(int) Math.pow(n, r);
+        float a = (float) 1/3;
+
+        int s =(int) Math.pow(n, a);
         return s;
     }
 }
